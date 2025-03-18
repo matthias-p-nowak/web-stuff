595497dfd8f8b9e999c2d133f443d3636dee2452
Matthias (generalsjefsutvikler) Nowak <matthiaspeter.nowak@centric.eu>
2025-03-18 10:30:46 +0100

    basic webapp - installable on mobile chrome

---
+++ web-app/serviceworker.js
@@ -0,0 +1,6 @@
+console.log('Service Worker Loaded again');
+const dt= new Date();
+const hr=dt.getHours();
+const mn=dt.getMinutes();
+const sc=dt.getSeconds();
+console.log('Time:',hr+':'+mn+':'+sc);
