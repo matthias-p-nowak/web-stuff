3945b017d77211c56b5aaf88a86dff9d42c84979
Matthias (generalsjefsutvikler) Nowak <matthiaspeter.nowak@centric.eu>
2025-03-21 14:21:42 +0100

    working more or less

--- workers/src/service-worker/service.ts
+++ workers/src/service-worker/service.ts
@@ -4,3 +4,4 @@ console.log('Hello, service worker!');
 let sw= <ServiceWorkerGlobalScope> <unknown>self ;
 sw.registration.showNotification('Hello, world! from service');
 console.log('Notification sent!');
+
